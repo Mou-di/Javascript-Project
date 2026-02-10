b3c47b7f32fdb8c0a2c06f54feee0b9fba31e79a
Mou-di <247495700+Mou-di@users.noreply.github.com>
2026-02-09 19:58:31 -0500

    Add files via upload
    Alab 308.1.1

---
+++ indexlab3.js
@@ -0,0 +1,23 @@
+
+
+// Count to from to 10
+for (let i = 1; i <= 10; i +=1) {
+	console.log(i);
+}
+
+// Even number 
+for (let r = 2; r <= 10; r += 2) {
+  console.log(r);  
+}
+
+for (let r = 6; r < 60; r += 3) {
+    console.log(r);
+    break;
+}
+for (let r = 1; r <= 7; r ++) {
+    console.log("Prime");
+}
+
+for (let r = 1; r <= 7; r ++) {
+    console.log("#".repeat(r));
+}
